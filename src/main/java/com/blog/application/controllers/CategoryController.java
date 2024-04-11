b4c142b94567e130ff9918555a393a9da2993673
Harender Singh <89127398+Harender215@users.noreply.github.com>
2024-04-11 16:49:50 +0530

    Creating Apis for categories of posts

---
+++ src/main/java/com/blog/application/controllers/CategoryController.java
@@ -0,0 +1,62 @@
+package com.blog.application.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.application.payloads.ApiResponse;
+import com.blog.application.payloads.CategoryDto;
+import com.blog.application.services.CategoryService;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+	
+	@Autowired
+	private CategoryService categoryService; 
+	//create
+	@PostMapping("/")
+	public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto) {
+		CategoryDto createCategory = this.categoryService.createCategory(categoryDto);
+		return new ResponseEntity<CategoryDto>(createCategory, HttpStatus.CREATED);
+	}
+	
+	//update
+	@PutMapping("/{categoryId}")
+	public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto, @PathVariable Integer categoryId) {
+		CategoryDto updatedCategory = this.categoryService.updateCategory(categoryDto, categoryId);
+		return new ResponseEntity<CategoryDto>(updatedCategory, HttpStatus.OK);
+	}
+	
+	
+	//delete
+	@DeleteMapping("/{categoryId}")
+	public ResponseEntity<ApiResponse> deleteCategory(@RequestBody CategoryDto categoryDto, @PathVariable Integer categoryId) {
+		this.categoryService.deleteCategory(categoryId);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("Category deleted successfully", true), HttpStatus.OK);
+	}
+	
+	//get
+	@GetMapping("/{categoryId}")
+	public ResponseEntity<CategoryDto> getCategory(@PathVariable Integer categoryId) {
+		CategoryDto category = this.categoryService.getCategory(categoryId);
+		return new ResponseEntity<CategoryDto>(category, HttpStatus.OK);
+	}
+	
+	//get all	
+	@GetMapping("/")
+	public ResponseEntity<List<CategoryDto>> getAllCategory() {
+		return ResponseEntity.ok(this.categoryService.getCategories());
+		
+	}
+}
---
+++ src/main/java/com/blog/application/repositories/CategoryRepo.java
@@ -0,0 +1,10 @@
+package com.blog.application.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blog.application.entities.Category;
+
+public interface CategoryRepo extends JpaRepository<Category, Integer>{
+	
+}
