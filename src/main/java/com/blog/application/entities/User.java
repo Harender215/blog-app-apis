ebdcc263793449bc17cdad192bf6196c3fd25ae1
Harender Singh <89127398+Harender215@users.noreply.github.com>
2024-04-11 19:24:03 +0530

    Added Entity class for Post

--- src/main/java/com/blog/application/entities/User.java
+++ src/main/java/com/blog/application/entities/User.java
@@ -1,9 +1,16 @@
 package com.blog.application.entities;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 
 import lombok.NoArgsConstructor;
@@ -28,4 +35,6 @@ public class User {
 	private String password;
 	private String about;
 
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<Post>();
 }
